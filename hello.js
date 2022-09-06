1957cf233c5955f06792966044d0e4a4dd708ac5
jordangm94 <jordan.guerrero.martinez@gmail.com>
2022-09-06 20:41:21 +0000

    hello.js

---
+++ hello.js
@@ -0,0 +1,14 @@
+/*const sayHello = function() {
+  console.log("Hello, World")
+}
+
+sayHello();
+*/
+
+const sayHello = function(name) {
+  console.log("Hello, " + name);
+}
+sayHello("Jordan");
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
